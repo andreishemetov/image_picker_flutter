a8208ba5bc97ef95ffaa38cec5a34d48da07404c
andrei.shemetau <andrei.shemetov@gmail.com>
2023-07-26 17:38:49 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  File? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image != null ? Image.file(_image!) : const Icon(Icons.image, size: 150,),
+            ElevatedButton(
+              onPressed: _chooseImages,
+              onLongPress: _chooseImages,
+              child: const Text('Choose / Capture'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _chooseImages(){
+
+  }
+
+  _captureImage(){
+
+  }
+}
