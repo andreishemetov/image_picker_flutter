167e72078dba9c3be5ef23be2fccead3b7327e48
andrei.shemetau <andrei.shemetov@gmail.com>
2023-07-31 11:42:03 +0200

    init labeling project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/bg.jpg'), fit: BoxFit.cover),
+          ),
+          child: const MyHomePage(title: 'Flutter Demo Home Page')),
     );
   }
 }
@@ -31,8 +37,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  late ImagePicker _imagePicker;
   File? _image;
+  String result = 'Results will be shown here';
+
+  @override
+  void initState() {
+    super.initState();
+    _imagePicker = ImagePicker();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,27 +53,91 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image != null ? Image.file(_image!) : const Icon(Icons.image, size: 150,),
-            ElevatedButton(
-              onPressed: _chooseImages,
-              onLongPress: _chooseImages,
-              child: const Text('Choose / Capture'),
+          children: [
+            const SizedBox(
+              width: 100,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 100),
+              child: Stack(children: <Widget>[
+                Stack(children: <Widget>[
+                  Center(
+                    child: Image.asset(
+                      'assets/images/frame.png',
+                      height: 510,
+                      width: 500,
+                    ),
+                  ),
+                ]),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent),
+                    onPressed: _imgFromGallery,
+                    onLongPress: _imgFromCamera,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: _image != null
+                          ? Image.file(
+                              _image!,
+                              width: 335,
+                              height: 495,
+                              fit: BoxFit.fill,
+                            )
+                          : Container(
+                              width: 340,
+                              height: 330,
+                              child: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.black,
+                                size: 100,
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: Text(
+                result,
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
             ),
           ],
         ),
       ),
+      backgroundColor: Colors.transparent,
     );
   }
 
-  _chooseImages(){
-
+  _imgFromCamera() async {
+    XFile? pickedFile = await _imagePicker.pickImage(source: ImageSource.camera);
+    _image = File(pickedFile!.path);
+    setState(() {
+      _image;
+      doImageLabeling();
+    });
   }
 
-  _captureImage(){
+  _imgFromGallery() async {
+    XFile? pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        doImageLabeling();
+      });
+    }
+  }
 
+  //TODO image labeling code here
+  doImageLabeling() async {
   }
 }
