6a2d92ac5d3616602f8e5674bedf38e8063585a8
andrei.shemetau <andrei.shemetov@gmail.com>
2023-09-29 11:00:42 +0200

    barcode init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+// import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,36 +33,138 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  late ImagePicker imagePicker;
   File? _image;
+  String result = 'results will be shown here';
+  // late BarcodeScanner barcodeScanner;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    imagePicker = ImagePicker();
+    //TODO initialize scanner
+    // barcodeScanner = BarcodeScanner(formats: [BarcodeFormat.all]);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  //TODO capture image using camera
+  _imgFromCamera() async {
+    XFile? pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
+    _image = File(pickedFile!.path);
+    setState(() {
+      _image;
+      doBarcodeScanning();
+    });
+  }
+
+  //TODO choose image using gallery
+  _imgFromGallery() async {
+    XFile? pickedFile =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        doBarcodeScanning();
+      });
+    }
+  }
+
+  //TODO barcode scanning code here
+  doBarcodeScanning() async {
+    // InputImage inputImage = InputImage.fromFile(_image!);
+    // final barcodes = await barcodeScanner.processImage(inputImage);
+
+    // for (Barcode barcode in barcodes) {
+    //   final BarcodeType type = barcode.type;
+    //   switch (type) {
+    //     case BarcodeType.url:
+    //       BarcodeUrl barcodeUrl = barcode.value as BarcodeUrl;
+    //       result = 'Url: ' + barcodeUrl.url!;
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // }
+    // setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image != null ? Image.file(_image!) : const Icon(Icons.image, size: 150,),
-            ElevatedButton(
-              onPressed: _chooseImages,
-              onLongPress: _chooseImages,
-              child: const Text('Choose / Capture'),
+    return MaterialApp(
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/bg.jpg'), fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: 100,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 100),
+                  child: Stack(children: <Widget>[
+                    Stack(children: <Widget>[
+                      Center(
+                        child: Image.asset(
+                          'images/frame.jpg',
+                          height: 350,
+                          width: 350,
+                        ),
+                      ),
+                    ]),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            shadowColor: Colors.transparent),
+                        onPressed: _imgFromGallery,
+                        onLongPress: _imgFromCamera,
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 12),
+                          child: _image != null
+                              ? Image.file(
+                                  _image!,
+                                  width: 325,
+                                  height: 325,
+                                  fit: BoxFit.fill,
+                                )
+                              : Container(
+                                  width: 340,
+                                  height: 330,
+                                  child: const Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.black,
+                                    size: 100,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+          backgroundColor: Colors.transparent,
         ),
       ),
     );
-  }
-
-  _chooseImages(){
-
-  }
-
-  _captureImage(){
-
   }
 }
